b4629f4a77b16bfbf53bdbcddceeaf8081d1b996
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2017-06-25 08:00:07 -0400

    added fix

--- run.sh
+++ run.sh
@@ -3,4 +3,11 @@
 if [ $OPENUNISON_KEYSTORE_PASSWORD ]; then
   export CATALINA_OPTS="$CATALINA_OPTS -DunisonKeystorePassword=$OPENUNISON_KEYSTORE_PASSWORD"
 fi
+
+mkdir -p /tmp/quartz
+
+#create server.xml
+/usr/local/tomcat/bin/eval_secrets.py /etc/openunison/ou.env /usr/local/tomcat/conf/server_template.xml > /usr/local/tomcat/conf/server.xml
+
+
 /usr/local/tomcat/bin/catalina.sh run
